a83ae8ae04a2e73f1805578ae82a9c8a0064971b
Gus <gustavomfreire@gmail.com>
2017-04-10 02:31:04 -0300

    Initial commit.

---
+++ ons.scala
@@ -0,0 +1,14 @@
+// Fonte:http://www.ons.org.br/historico/geracao_energia.aspx
+// Formatar dados  para .csv contendo os períodes entre 2017 e 2002 ou o desejado
+// Observar em 2002 -  " A crise do apagao"
+
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
+val data = spark.read.option("header","true").option("inferSchema","true").csv("ons.csv")
+    println("")
+    println("")
+    println("--------+  Geracao de Energia - Hidráulica - GW/h    ")
+    println("--------+  SIN   - 2017 - 2002                       ")
+    println("--------+  Fonte: ONS                                ")
+    println("")
+data.show()
