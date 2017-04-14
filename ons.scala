146c0f40250274f4d8e352c30ed3c0765c19f951
Gus <gustavomfreire@gmail.com>
2017-04-14 02:52:50 -0300

    ml

--- ons.scala
+++ ons.scala
@@ -3,14 +3,21 @@
 // Observar em 2002 -  " A crise do apagao"
 
 import org.apache.spark.sql.SparkSession
-val spark = SparkSession.builder().getOrCreate()
-val data = spark.read.option("header","true").option("inferSchema","true").csv("ons.csv")
-    println("")
-    println("")
-    println("--------+  Geracao de Energia - Hidráulica - GW/h    ")
-    println("--------+  SIN   - 2017 - 2002                       ")
-    println("--------+  Fonte: ONS                                ")
-    println("")
-data.show()
-//Filtrando Linha MES Fevereiro
-data.filter($"Mês" === "Fev").show()
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+
+val spark = SparkSession.builder().getOrCreate();
+val data = spark.read.option("header","true").option("inferSchema","true").csv("geracao-sin-hidraulica-2017-2002.csv");
+//formatacao inicial
+    println("");println("");
+    println("--------+  Geracao de Energia - Hidráulica - GW/h    ");
+    println("--------+  SIN   - 2017 - 2002                       ");
+    println("--------+  Fonte: ONS                                ");println("");
+// data.printSchema()  Check Schema type // data.columns mostra array de colunas
+  data.show();println("");
+  println("Estatistica Basica ( stddev, mean, count, max, min)");println("");data.describe().show();
+  println("Mes de Maio");println("");data.select($"Ano",$"5").show() //Seleciona coluna 5
+  println("Mes de Maio acima de 35000 GWh/h");println("");data.filter($"5" > 35000).show(); //Mes Maio com producao acima de 35000 GW/h
+  println("Melhores mes de maio");println("");data.filter($"5" > 35000).show(); //Mes Maio com producao acima de 35000 GW/h
+//vector assembler for ml
+val data = data.select(data("Ano").as("label"),$"1",$"2",$"3",$"4",$"5",$"6",$"7",$"8",$"9",$"10",$"11",$"12")
